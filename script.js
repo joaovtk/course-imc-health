f5b081d822b75ad7fc955602cd1fcd5effef0c71
Proz <joaovitorsoaresdosreis3@gmail.com>
2023-12-18 17:14:15 -0300

    Deploy

--- script.js
+++ script.js
@@ -4,28 +4,33 @@
         e.preventDefault();
         let weight = document.getElementById("peso").value;
         let height = document.getElementById("altura").value;
+        weight = weight.replace(",", ".");
+        height = height.replace(",", ".");
         let imc = Number((weight / height ** 2)).toFixed(1)
-
-        if(isNaN(imc)){
-            alert("Digite um valor valido")
+        if(!weight || !height){
+            alert("Valores invalidos");
         }else {
-            document.querySelector(".box > h1").textContent = imc + " pontos";
-            let words = document.querySelector(".box > p");
-            if(imc < 18.5){
-                words.textContent = "Abaixo do peso";
-            }else if(imc < 24.9){
-                words.textContent = "Peso Ideal";
-            }else if(imc < 29.9){
-                words.textContent = "Lemente acima do peso";
-            }else if(imc < 34.9){
-                words.textContent = "Obesidade grau 1";
-            }else if(imc < 39.9){
-                words.textContent = "Obsediade 2 severa";
+            if(isNaN(imc)){
+                alert("Digite um valor valido");
             }else {
-                words.textContent = "Obesidade morbida";
+                document.querySelector(".box > h1").textContent = imc + " pontos";
+                let words = document.querySelector(".box > p");
+                if(imc < 18.5){
+                    words.textContent = "Abaixo do peso";
+                }else if(imc < 24.9){
+                    words.textContent = "Peso Ideal";
+                }else if(imc < 29.9){
+                    words.textContent = "Lemente acima do peso";
+                }else if(imc < 34.9){
+                    words.textContent = "Obesidade grau 1";
+                }else if(imc < 39.9){
+                    words.textContent = "Obsediade 2 severa";
+                }else {
+                    words.textContent = "Obesidade morbida";
+                }
             }
+    
+            form.reset();
         }
-
-        form.reset();
     });
 })();
